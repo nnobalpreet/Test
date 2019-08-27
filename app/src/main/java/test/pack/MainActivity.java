b3bd4fcb958bd5a21c0145b19df8886e678642c6
Nobalpreet Nobalpreet <39172933+nnobalpreet@users.noreply.github.com>
2019-08-27 12:30:58 -0400

    secret key and imports

--- app/src/main/java/test/pack/MainActivity.java
+++ app/src/main/java/test/pack/MainActivity.java
@@ -3,6 +3,9 @@ package test.pack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "5993bb33-e9fd-44b1-b5b4-941fc8518d7e",
+                  Analytics.class, Crashes.class);
     }
 }
